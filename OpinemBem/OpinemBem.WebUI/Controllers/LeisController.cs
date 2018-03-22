3b37fe3b0797b118914b218189a7e1f3e5ec3b44
IsabelaSucharski <isasucharski@hotmail.com>
2018-03-21 22:20:19 -0300

    criando telas

---
+++ OpinemBem/OpinemBem.WebUI/Controllers/LeisController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OpinemBem.WebUI.Controllers
+{
+    public class LeisController : Controller
+    {
+        public ActionResult AceitarLeisAdm()
+        {
+            return View();
+        }
+
+        public ActionResult ConcordaLeisAdm()
+        {
+            return View();
+        }
+
+        public ActionResult ConcordaLeisU()
+        {
+            return View();
+        }
+
+        public ActionResult GerenciarLeisAdm()
+        {
+            return View();
+        }
+    }
+}
---
+++ OpinemBem/OpinemBem.WebUI/Controllers/ProjetosController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OpinemBem.WebUI.Controllers
+{
+    public class ProjetosController : Controller
+    {
+        public ActionResult AlterarProjetos()
+        {
+            return View();
+        }
+
+        public ActionResult CadProjetoAdm()
+        {
+            return View();
+        }
+
+        public ActionResult CadProjetos()
+        {
+            return View();
+        }
+
+        public ActionResult ProjetoAdm()
+        {
+            return View();
+        }
+
+        public ActionResult ProjetoU()
+        {
+            return View();
+        }
+    }
+}
