34ce58b5208dc96d8dd0954c7ac8e9acf62973c8
Tiago Andrade <tgnandrade@gmail.com>
2018-03-29 10:50:27 -0300

    criando classe DAO de exemplo
    Identification: 14
    Change-Id: I13cf9f09dd63a0c54b10f25e6dcd9f7eac624d10

--- OpinemBem/OpinemBem.DataAccess/CategoriaDAO.cs
+++ OpinemBem/OpinemBem.DataAccess/CategoriaDAO.cs
@@ -1,11 +1,165 @@
-﻿using System;
+﻿using OpinemBem.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace OpinemBem.DataAccess
 {
     public class CategoriaDAO
     {
+        public void Inserir(Categoria obj)
+        {
+            //Criando uma conexão com o banco de dados
+            using (SqlConnection conn = new SqlConnection(@"Initial Catalog=OpinemBem; Data Source=localhost; Integrated Security=SSPI;"))
+            {
+                //Criando instrução sql para inserir na tabela de categorias
+                string strSQL = @"INSERT INTO categoria (nome) VALUES (@nome);";
+
+                //Criando um comando sql que será executado na base de dados
+                using (SqlCommand cmd = new SqlCommand(strSQL))
+                {
+                    cmd.Connection = conn;
+                    //Preenchendo os parâmetros da instrução sql
+                    cmd.Parameters.Add("@nome", SqlDbType.VarChar).Value = obj.Nome;
+
+                    //Abrindo conexão com o banco de dados
+                    conn.Open();
+                    //Executando instrução sql
+                    cmd.ExecuteNonQuery();
+                    //Fechando conexão com o banco de dados
+                    conn.Close();
+                }
+            }
+        }
+
+        public void Atualizar(Categoria obj)
+        {
+            //Criando uma conexão com o banco de dados
+            using (SqlConnection conn = new SqlConnection(@"Initial Catalog=OpinemBem; Data Source=localhost; Integrated Security=SSPI;"))
+            {
+                //Criando instrução sql para inserir na tabela de categorias
+                string strSQL = @"UPDATE categoria set nome = @nome where id_categoria = @id_categoria;";
+
+                //Criando um comando sql que será executado na base de dados
+                using (SqlCommand cmd = new SqlCommand(strSQL))
+                {
+                    cmd.Connection = conn;
+                    //Preenchendo os parâmetros da instrução sql
+                    cmd.Parameters.Add("@nome", SqlDbType.VarChar).Value = obj.Nome;
+                    cmd.Parameters.Add("@id_categoria", SqlDbType.VarChar).Value = obj.Id;
+
+                    //Abrindo conexão com o banco de dados
+                    conn.Open();
+                    //Executando instrução sql
+                    cmd.ExecuteNonQuery();
+                    //Fechando conexão com o banco de dados
+                    conn.Close();
+                }
+            }
+        }
+
+        public void Deletar(Categoria obj)
+        {
+            //Criando uma conexão com o banco de dados
+            using (SqlConnection conn = new SqlConnection(@"Initial Catalog=OpinemBem; Data Source=localhost; Integrated Security=SSPI;"))
+            {
+                //Criando instrução sql para inserir na tabela de categorias
+                string strSQL = @"DELETE FROM categoria where id_categoria = @id_categoria;";
+
+                //Criando um comando sql que será executado na base de dados
+                using (SqlCommand cmd = new SqlCommand(strSQL))
+                {
+                    cmd.Connection = conn;
+                    //Preenchendo os parâmetros da instrução sql
+                    cmd.Parameters.Add("@id_categoria", SqlDbType.VarChar).Value = obj.Id;
+
+                    //Abrindo conexão com o banco de dados
+                    conn.Open();
+                    //Executando instrução sql
+                    cmd.ExecuteNonQuery();
+                    //Fechando conexão com o banco de dados
+                    conn.Close();
+                }
+            }
+        }
+
+        public Categoria BuscarPorId(int id)
+        {
+            //Criando uma conexão com o banco de dados
+            using (SqlConnection conn = new SqlConnection(@"Initial Catalog=OpinemBem; Data Source=localhost; Integrated Security=SSPI;"))
+            {
+                //Criando instrução sql para selecionar todos os registros na tabela de Categorias
+                string strSQL = @"SELECT * FROM categoria where id_categoria = @id_categoria;";
+
+                //Criando um comando sql que será executado na base de dados
+                using (SqlCommand cmd = new SqlCommand(strSQL))
+                {
+                    //Abrindo conexão com o banco de dados
+                    conn.Open();
+                    cmd.Connection = conn;
+                    cmd.CommandText = strSQL;
+                    //Executando instrução sql
+                    var dataReader = cmd.ExecuteReader();
+                    var dt = new DataTable();
+                    dt.Load(dataReader);
+                    //Fechando conexão com o banco de dados
+                    conn.Close();
+
+                    if (!(dt != null && dt.Rows.Count > 0))
+                        return null;
+
+                    var row = dt.Rows[0];
+                    var categoria = new Categoria()
+                    {
+                        Id = Convert.ToInt32(row["id"]),
+                        Nome = row["nome"].ToString()
+                    };
+
+                    return categoria;
+                }
+            }
+        }
+
+        public List<Categoria> BuscarTodos()
+        {
+            var lst = new List<Categoria>();
+
+            //Criando uma conexão com o banco de dados
+            using (SqlConnection conn = new SqlConnection(@"Initial Catalog=OpinemBem; Data Source=localhost; Integrated Security=SSPI;"))
+            {
+                //Criando instrução sql para selecionar todos os registros na tabela de Categorias
+                string strSQL = @"SELECT * FROM categoria;";
+
+                //Criando um comando sql que será executado na base de dados
+                using (SqlCommand cmd = new SqlCommand(strSQL))
+                {
+                    //Abrindo conexão com o banco de dados
+                    conn.Open();
+                    cmd.Connection = conn;
+                    cmd.CommandText = strSQL;
+                    //Executando instrução sql
+                    var dataReader = cmd.ExecuteReader();
+                    var dt = new DataTable();
+                    dt.Load(dataReader);
+                    //Fechando conexão com o banco de dados
+                    conn.Close();
+
+                    //Percorrendo todos os registros encontrados na base de dados e adicionando em uma lista
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        var Categoria = new Categoria()
+                        {
+                            Id = Convert.ToInt32(row["id"]),
+                            Nome = row["nome"].ToString()
+                        };
+
+                        lst.Add(Categoria);
+                    }
+                }
+            }
+
+            return lst;
+        }
     }
 }
