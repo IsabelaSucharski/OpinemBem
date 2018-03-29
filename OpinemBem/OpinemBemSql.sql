9da6953096c9b98663a44de843cf7039f6676f46
Tiago Andrade <tgnandrade@gmail.com>
2018-03-29 01:11:13 -0300

    script de banco de dados
    Identification: 12
    Change-Id: I5848585774b1037e5fd17c96a5e1a884e0753190

--- OpinemBem/OpinemBemSql.sql
+++ OpinemBem/OpinemBemSql.sql
@@ -1,77 +1,58 @@
+use master
+go
+
 create database OpinemBem
+go
+
 use OpinemBem
-
-
-create table tipoUsuario -- 1..n com usuario
-(
-	idTipo int primary key identity(1,1),
-	nome varchar(100)
-);
-
+go
 
 create table usuario
 (
-	idUsuario int identity(1,1) primary key,
-	nome varchar(100),
+	id_usuario int identity(1,1) primary key,
+	nome varchar(200) not null,
+	cpf varchar(20) not null unique,
+	email varchar(150) not null,
+	senha varchar(100) not null,
 	dataNasc datetime,
-	cpf varchar(15) unique,
-	email varchar(50),
-	--tipoUsuario int (eNUM NO C#)
-	senha varchar(20)
-
-)
-
-
-create table administrador
-(
-	idAdmin int identity(1,1) primary key,
-	nome varchar(100),
-	dataNasc datetime,
-	cpf varchar(15) unique,
-	email varchar(50),
-	--tipoUsuario int references 
-	senha varchar(20),
-	administrador char(1)
-)
-
+	administrador bit
+);
 
 create table categoria
 (
-	idCategoria int identity(1,1) primary key,
-	nome varchar(100)	
-)
+	id_categoria int identity(1,1) primary key,
+	nome varchar(200)	
+);
 
-
-create table projetoDeLei
+create table projeto_de_lei
 (
-	idProjeto int identity(1,1) primary key,
-	nome varchar(300),
+	id_projeto int identity(1,1) primary key,
+	nome varchar(500),
+	id_categoria int not null references categoria (id_categoria),
 	descricao varchar(max),
 	vantagens varchar(max),
-	desvantagens varchar(max),
-	idCategoria int references categoria(idCategoria)
-)
+	desvantagens varchar(max)
+);
 
-
-create table voto --1..n com administrador e usuario
+create table voto
 (
-idUsuario int references usuario,
-idProjeto int references projetoDeLei,
-constraint pk_proj_usu primary key (idusuario,idprojeto),
-voto char,
-votado bit,
-data_voto datetime not null default getdate()
-)
+	id_usuario int not null references usuario,
+	id_projeto int not null references projeto_de_lei,
+	data_voto datetime not null default getdate(),
+	constraint pk_voto primary key (id_usuario, id_projeto)
+);
 
+create table comentario
+(
+	id_comentario int identity(1,1) primary key,
+	id_usuario int not null references usuario,
+	id_projeto int not null references projeto_de_lei,
+	data_comentario datetime not null default getdate(),
+	mensagem varchar(max)
+);
 
-	--sexo int (eNUM NO C#),
-	--frentepli varchar(250),
-	--religiao varchar(250),
-	--email varchar(200) unique,
-	--nivel int
-
-
-
-
-
-
+--sexo int (eNUM NO C#),
+--frentepli varchar(250),
+--religiao varchar(250),
+--email varchar(200) unique,
+--nivel int
