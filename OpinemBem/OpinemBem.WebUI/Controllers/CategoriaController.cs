ba41e8735fb2daa69c02fdfb67939f3734da3183
IsabelaSucharski <isasucharski@hotmail.com>
2018-04-18 22:30:18 -0300

    ARRUMANDO

--- OpinemBem/OpinemBem.WebUI/Controllers/CategoriaController.cs
+++ OpinemBem/OpinemBem.WebUI/Controllers/CategoriaController.cs
@@ -10,24 +10,22 @@ namespace OpinemBem.WebUI.Controllers
 {
     public class CategoriaController : Controller
     {
-        // GET: Categoria
         public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult SalvarCategoria(Categoria obj)
-        {
-            new CategoriaDAO().Inserir(obj);
-            return RedirectToAction("Index", "Categoria"); 
-        }
-
-        public ActionResult Cadastro()
         {
             var lst = new CategoriaDAO().BuscarTodos();
             return View(lst);
         }
 
+        public ActionResult SalvarCategoria(Categoria obj)
+        {
+            new CategoriaDAO().Inserir(obj);
 
+            return RedirectToAction("Index", "Categoria");
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
     }
 }
