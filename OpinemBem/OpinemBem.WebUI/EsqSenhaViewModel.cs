61b9b2c9729a2b21d6ede8cce4c7bc955d687a5f
Isabela Sucahrski <isasucharski@hotmail.com>
2018-05-01 15:18:31 -0300

    criando esquecer lei

---
+++ OpinemBem/OpinemBem.WebUI/EsqSenhaViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OpinemBem.WebUI
+{
+    public class EsqSenhaViewModel
+    {
+
+        [Required(ErrorMessage = "Campo 'Email' é obrigatório!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo 'Senha' é obrigatório!")]
+        public string Senha { get; set; }
+
+        public string ConfirmaSenha { get; set; }
+    }
+}
