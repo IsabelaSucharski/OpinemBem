e779600d8310be6e303432f252dbc328a4eb988d
IsabelaSucharski <isasucharski@hotmail.com>
2018-06-08 22:23:33 -0300

    criando testes

---
+++ OpinemBem/TesteCategoria/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpinemBem.Models;
+
+namespace TesteCategoria
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //instanciar objeto
+            Categoria obj = new Categoria();
+            //escolher o metodo
+            obj.Inserir();                        
+        }
+    }
+}
